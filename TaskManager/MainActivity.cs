36ceef824595b17ba3e026355980adacd1e8992d
udayteja96 <udaytejaganesuni@gmail.com>
2018-12-19 07:29:42 +0530

    Add project files.

---
+++ TaskManager/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.Design.Widget;
+using TaskManager.Model;
+using System.Collections.Generic;
+using TaskManager.Service;
+using System.Linq;
+
+namespace TaskManager
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public List<Task> RawTasks { get; set; }
+        private TaskService TaskService { get; set; }
+        public ListView TaskList { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            TaskService = new TaskService();
+            TaskService.AddTask(new Task() { Name = "First", Description = "First Task" });
+            TaskService.AddTask(new Task() { Name = "Second", Description = "Second Task" });
+            TaskService.AddTask(new Task() { Name = "Third", Description = "Third Task" });
+            RawTasks = TaskService.GetAll();
+
+            TaskList = FindViewById<ListView>(Resource.Id.mainlistview); 
+            TaskList.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, RawTasks);
+            TaskList.ItemClick += (s, e) =>
+            {
+                var t = RawTasks.FirstOrDefault(p => p.Id == e.Id + 1);
+                Toast.MakeText(this, t.Name, ToastLength.Long).Show();
+            };
+
+            var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
+
+            bottomNavigation.NavigationItemSelected += (s, e) =>
+            {
+                switch (e.Item.ItemId)
+                {
+                    case Resource.Id.action_new:
+                        Toast.MakeText(this, "New Clicked", ToastLength.Short).Show();
+                        RawTasks.Where(t => t.Status == Status.New);
+                        break;
+                    case Resource.Id.action_in_progress:
+                        Toast.MakeText(this, "In Progress Clicked", ToastLength.Short).Show();
+                        RawTasks.Where(t => t.Status == Status.InProgress);
+                        break;
+                    case Resource.Id.action_completed:
+                        Toast.MakeText(this, "Completed Clicked", ToastLength.Short).Show();
+                        RawTasks.Where(t => t.Status == Status.Completed);
+                        break;
+                    default:
+                        break;
+                }
+            };
+        }
+    }
+}
---
+++ TaskManager/Service/TaskService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using TaskManager.Model;
+using Environment = System.Environment;
+
+namespace TaskManager.Service
+{
+    public class TaskService
+    {
+        string databaseFileName;
+
+        public TaskService()
+        {
+            string applicationFolderPath = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "CanFindLocation");
+
+            // Create the folder path.
+            System.IO.Directory.CreateDirectory(applicationFolderPath);
+
+            databaseFileName = System.IO.Path.Combine(applicationFolderPath, "CanFindLocation.db");
+            var db = new SQLiteConnection(databaseFileName);
+
+            db.CreateTable<Task>();
+        }
+
+        public void AddTask(Task task)
+        {
+            using (var db = new SQLiteConnection(databaseFileName))
+            {
+                db.Insert(task);
+            }
+        }
+
+        public void UpdateTask(Task task)
+        {
+            using (var db = new SQLiteConnection(databaseFileName))
+            {
+                db.Update(task);
+            }
+        }
+
+        public void DeleteTask(Task task)
+        {
+            using (var db = new SQLiteConnection(databaseFileName))
+            {
+                db.Delete(task);
+            }
+        }
+
+        public List<Task> GetAll()
+        {
+            List<Task> tasks;
+            using (var db = new SQLiteConnection(databaseFileName))
+            {
+                tasks = new List<Task>(db.Table<Task>());
+            }
+            return tasks;
+        }
+
+        public Task Get(Task task)
+        {
+            Task Task;
+
+            using (var db = new SQLiteConnection(databaseFileName))
+            {
+                Task = db.Get<Task>(task.Id);
+            }
+
+            return Task;
+        }
+    }
+}
